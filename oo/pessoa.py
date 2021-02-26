eafb83af2b74b3b625e48e2e8f532099ddc4561a
MICRO <134sandrinha@gmail.com>
2021-02-26 08:34:38 -0300

    criado metodocomprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def  cumprimentar (self):
+        return f' ola {id(self)} '
+
+if __name__ == '__main__':
+    P = Pessoa()
+    print(Pessoa.cumprimentar(P))
+    print(id(P))
+    print(P.cumprimentar())
