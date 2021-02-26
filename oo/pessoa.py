7119999a403dc9c7a8a1eb32147e59de4852fade
MICRO <134sandrinha@gmail.com>
2021-02-26 09:10:23 -0300

    criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade= 19):
+        self.idade = idade
+        self.nome = nome
     def  cumprimentar (self):
         return f' ola {id(self)} '
 
 if __name__ == '__main__':
-    P = Pessoa()
+    P = Pessoa('lucas')
     print(Pessoa.cumprimentar(P))
     print(id(P))
     print(P.cumprimentar())
+    print(P.nome)
+    P.nome='casa'
+    print(P.nome)
+    print(P.idade)
